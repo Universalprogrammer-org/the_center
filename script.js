48c14aaf4bfa7a02b7a7b8ff3e6efec5a0b653c9
Universalprogrammer <brunnostudios164@gmail.com>
2025-03-31 18:04:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-/*Versión 1.0.1.1*/
+var timepass = document.getElementById("Time");
+function Time() {
+    setInterval(function(){ var time = new Date(); timepass.textContent = time.getHours() + ":" + time.getMinutes() }, 1000); 
+}
+Time();
+/*Versión 1.0.2.1*/
